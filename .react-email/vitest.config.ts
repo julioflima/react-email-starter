8e40885efe43984949d7a7aeaf8d5fbfeed45f58
Julio Lima <julio_flima@hotmail.com>
2025-11-06 23:31:26 -0500

    style: standardize quotes and add resolve configuration in vitest config

--- .react-email/vitest.config.ts
+++ .react-email/vitest.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    noExternal: [/^@react-email\//], // Add regex here
+  },
   test: {
     globals: true,
-    environment: 'happy-dom',
+    environment: "happy-dom",
   },
   esbuild: {
     tsconfigRaw: {
       compilerOptions: {
-        jsx: 'react-jsx',
+        jsx: "react-jsx",
       },
     },
   },
